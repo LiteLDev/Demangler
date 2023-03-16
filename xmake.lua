f04f91dcc2cf2d6cc9e744896cb24ea03db12b13
RimuruChan <RealSprite233@outlook.com>
2023-03-16 08:12:55 +0800

    build: remove some cxx flags

--- xmake.lua
+++ xmake.lua
@@ -1,19 +1,15 @@
 add_rules("mode.debug", "mode.release")
 
+if is_mode("debug") then
+    set_runtimes("MDd")
+else
+    set_runtimes("MD")
+end
+
 target("Demangler")
     set_kind("static")
     set_languages("c++17")
     add_headerfiles("include/(**.h)", "include/(**.def)")
     add_includedirs("./include")
-    add_cxxflags("/UTF-8", "/GL", "/permissive-")
-    if is_mode("debug") then
-        add_defines("DEBUG")
-        add_cxxflags("/MDd")
-        add_ldflags("/DEBUG")
-    else
-        add_defines("NDEBUG")
-        add_cxxflags("/MD")
-        add_ldflags("/OPT:REF", "/OPT:ICF")
-    end
-    add_ldflags("/LTCG")
+    add_cxxflags("/UTF-8", "/permissive-")
     add_files("src/**.cpp")
